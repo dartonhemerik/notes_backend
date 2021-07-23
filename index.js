4f1a62eb28dd711c8d5796e9e22a2d2746d8d925
Darton Hemerik <73367214+dartonhemerik@users.noreply.github.com>
2021-07-22 18:05:26 -0700

    Moved notes frontend build folder to backend repository

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
-// const http = require('http');
-
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'text/plain' });
-//   response.end('Hello World');
-// });
-
 const express = require('express');
 const app = express();
 const cors = require('cors');
 
 app.use(express.json());
 app.use(cors());
-
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-];
-// const app = http.createServer((request, response) => {
-//   response.writeHead(200, { 'Content-Type': 'application/json' });
-//   response.end(JSON.stringify(notes));
-// });
+app.use(express.static('build'));
 
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
@@ -51,12 +20,9 @@ app.get('/api/notes', (request, response) => {
 });
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id);
-  // console.log(id);
   const note = notes.find((note) => {
-    // console.log(note.id, typeof note.id, id, typeof id, note.id === id);
     return note.id === id;
   });
-  // console.log(note);
   if (note) {
     response.json(note);
   } else {
@@ -102,8 +68,6 @@ const requestLogger = (request, response, next) => {
 app.use(requestLogger);
 
 const PORT = process.env.PORT || 3001;
-// app.listen(PORT);
-// console.log(`Server running on port ${PORT}`);
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
