a198696599fa0529f8af2d9921040bb7a8de78d1
Darton Hemerik <73367214+dartonhemerik@users.noreply.github.com>
2021-07-22 18:52:37 -0700

    Moved notes array to index.js, which allows the app to work

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
 
+const notes = [
+  {
+    id: 1,
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
+    important: true,
+  },
+  {
+    id: 2,
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
+    important: false,
+  },
+  {
+    id: 3,
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true,
+  },
+];
+
 const generateId = () => {
   const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
   return maxId + 1;
