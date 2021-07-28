2cafd304642ea4e8328d46bfa8c11016f6238b73
Darton Hemerik <73367214+dartonhemerik@users.noreply.github.com>
2021-07-27 19:36:18 -0700

    Successfully connected backend to MongoDB

--- index.js
+++ index.js
@@ -1,54 +1,32 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+require('dotenv').config();
+const Note = require('./models/note');
 
 app.use(express.json());
 app.use(cors());
 app.use(express.static('build'));
 
-const notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2019-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only JavaScript',
-    date: '2019-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2019-05-30T19:20:14.298Z',
-    important: true,
-  },
-];
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-  return maxId + 1;
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
 };
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+app.use(requestLogger);
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes);
+  Note.find({}).then((notes) => {
+    response.json(notes);
+  });
 });
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id);
-  const note = notes.find((note) => {
-    return note.id === id;
-  });
-  if (note) {
+  Note.findById(request.params.id).then((note) => {
     response.json(note);
-  } else {
-    response.status(404).end();
-  }
+  });
 });
 
 app.post('/api/notes', (request, response) => {
@@ -60,16 +38,15 @@ app.post('/api/notes', (request, response) => {
     });
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
-    important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  };
+    important: body.important || false,
+  });
 
-  notes = notes.concat(note);
-
-  response.json(note);
+  note.save().then((savedNote) => {
+    response.json(savedNote);
+  });
 });
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -78,15 +55,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end();
 });
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
-
-app.use(requestLogger);
+// app.use(unknownEndpoint);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
